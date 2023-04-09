e0d05b089ca1f352f941d69d9638d0cae9e3686a
TN-2812 <ngab1910668@student.ctu.edu.vn>
2023-04-09 18:29:01 +0700

    Thử test

--- QuanLyCauLacBo/Form1.Designer.cs
+++ QuanLyCauLacBo/Form1.Designer.cs
@@ -34,6 +34,8 @@
             txtTendangnhap = new TextBox();
             txtMatkhau = new TextBox();
             btnDangnhap = new Button();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            flowLayoutPanel1.SuspendLayout();
             SuspendLayout();
             // 
             // label2
@@ -43,10 +45,9 @@
             label2.FlatStyle = FlatStyle.Flat;
             label2.Font = new Font("Arial", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
             label2.ForeColor = Color.White;
-            label2.Location = new Point(416, 208);
-            label2.Margin = new Padding(5, 0, 5, 0);
+            label2.Location = new Point(3, 0);
             label2.Name = "label2";
-            label2.Size = new Size(216, 33);
+            label2.Size = new Size(126, 19);
             label2.TabIndex = 1;
             label2.Text = "Tên đăng nhập";
             // 
@@ -57,29 +58,26 @@
             label1.FlatStyle = FlatStyle.Flat;
             label1.Font = new Font("Arial", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
             label1.ForeColor = Color.White;
-            label1.Location = new Point(416, 293);
-            label1.Margin = new Padding(5, 0, 5, 0);
+            label1.Location = new Point(135, 0);
             label1.Name = "label1";
-            label1.Size = new Size(139, 33);
+            label1.Size = new Size(80, 19);
             label1.TabIndex = 2;
             label1.Text = "Mật khẩu";
             // 
             // txtTendangnhap
             // 
             txtTendangnhap.BackColor = Color.White;
-            txtTendangnhap.Location = new Point(622, 208);
-            txtTendangnhap.Margin = new Padding(5);
+            txtTendangnhap.Location = new Point(3, 22);
             txtTendangnhap.Name = "txtTendangnhap";
-            txtTendangnhap.Size = new Size(270, 39);
+            txtTendangnhap.Size = new Size(168, 27);
             txtTendangnhap.TabIndex = 3;
             txtTendangnhap.TextChanged += txtTendangnhap_TextChanged;
             // 
             // txtMatkhau
             // 
-            txtMatkhau.Location = new Point(622, 293);
-            txtMatkhau.Margin = new Padding(5);
+            txtMatkhau.Location = new Point(177, 22);
             txtMatkhau.Name = "txtMatkhau";
-            txtMatkhau.Size = new Size(270, 39);
+            txtMatkhau.Size = new Size(168, 27);
             txtMatkhau.TabIndex = 4;
             // 
             // btnDangnhap
@@ -89,35 +87,42 @@
             btnDangnhap.FlatStyle = FlatStyle.Flat;
             btnDangnhap.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
             btnDangnhap.ForeColor = SystemColors.ButtonFace;
-            btnDangnhap.Location = new Point(416, 382);
-            btnDangnhap.Margin = new Padding(5);
+            btnDangnhap.Location = new Point(3, 55);
             btnDangnhap.Name = "btnDangnhap";
-            btnDangnhap.Size = new Size(479, 62);
+            btnDangnhap.Size = new Size(295, 39);
             btnDangnhap.TabIndex = 5;
             btnDangnhap.Text = "Đăng nhập";
             btnDangnhap.TextImageRelation = TextImageRelation.ImageAboveText;
             btnDangnhap.UseVisualStyleBackColor = false;
             btnDangnhap.Click += btnDangnhap_Click;
             // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Controls.Add(label2);
+            flowLayoutPanel1.Controls.Add(label1);
+            flowLayoutPanel1.Controls.Add(txtTendangnhap);
+            flowLayoutPanel1.Controls.Add(txtMatkhau);
+            flowLayoutPanel1.Controls.Add(btnDangnhap);
+            flowLayoutPanel1.Location = new Point(248, 123);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(367, 168);
+            flowLayoutPanel1.TabIndex = 6;
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ActiveBorder;
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
             BackgroundImageLayout = ImageLayout.Center;
-            ClientSize = new Size(1271, 725);
-            Controls.Add(btnDangnhap);
-            Controls.Add(txtMatkhau);
-            Controls.Add(txtTendangnhap);
-            Controls.Add(label1);
-            Controls.Add(label2);
-            Margin = new Padding(5);
+            ClientSize = new Size(782, 453);
+            Controls.Add(flowLayoutPanel1);
             Name = "Form1";
             Text = "Đăng nhập";
             Load += Form1_Load;
+            flowLayoutPanel1.ResumeLayout(false);
+            flowLayoutPanel1.PerformLayout();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
@@ -126,5 +131,6 @@
         private TextBox txtTendangnhap;
         private TextBox txtMatkhau;
         private Button btnDangnhap;
+        private FlowLayoutPanel flowLayoutPanel1;
     }
 }
